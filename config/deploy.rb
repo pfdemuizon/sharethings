b627ee6036a08e5d5455e1ba35d7fd156c2c23c1
Patrice de Muizon <patrice@radicaldesigns.org>
2008-10-09 15:25:39 -0700

    fix cap deploy (add debugging) and remove haml for now.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "sharethings"
-set :repository,  "git@github.com:pfdemuizon/sharethings"
+set :repository,  "git@github.com:pfdemuizon/sharethings.git"
 
 set :user, "pfdemuizon"
 set :group, "pfdemuizon"
@@ -21,5 +21,9 @@ role :db,  "graspbirdstail.com", :primary => true
 namespace :deploy do
   task :symlink_shared, :roles => :app, :except => {:no_symlink => true} do
     invoke_command "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  task :version do
+    invoke_command "git --version"
+    invoke_command "echo $PATH"
   end
 end
