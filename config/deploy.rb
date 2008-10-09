6f33634dfb18187b6b287bbe74bcd266bdce06a1
Patrice de Muizon <patrice@radicaldesigns.org>
2008-10-09 15:15:20 -0700

    get cap deploy working

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,25 @@
 set :application, "sharethings"
 set :repository,  "git@github.com:pfdemuizon/sharethings"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :user, "pfdemuizon"
+set :group, "pfdemuizon"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
+# use git
 set :scm, :git
+set :git_enable_submodules, 1
 
-role :app, "sharethings.graspbirdstail.com"
-role :web, "sharethings.graspbirdstail.com"
-role :db,  "sharethings.graspbirdstail.com", :primary => true
+set :deploy_to, "/home/pfdemuizon/#{application}"
+
+set :deploy_via, :remote_cache
+set :keep_releases, 10
+set :use_sudo, false
+
+role :app, "graspbirdstail.com"
+role :web, "graspbirdstail.com"
+role :db,  "graspbirdstail.com", :primary => true
+
+namespace :deploy do
+  task :symlink_shared, :roles => :app, :except => {:no_symlink => true} do
+    invoke_command "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
